253c6ad168118d96f41f11023c2499c572feafcc
alan <paolanavas97@gmail.com>
2025-03-10 11:15:49 -0500

    ejercicios de practica

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -26,7 +26,7 @@ Jerarquia de precedencia de operadores
 2.      **
 3.      *, /, %,
 4.      +, -
-5.     >, <, >=, <=, !=,
+5.     >, <, >=, <=, !=, ==
 6.      not
 7.      and
 8.      or
@@ -39,3 +39,15 @@ print ("El resultado de operar con jerarquia es", y)
 
 #ejemplo 5: operadores relacionales y logicos 
 y= not 3 > 4 and 4 == 4 or 3 < 2
+
+#ejemplo 6: operadores aritmeticos
+#relacionales y logicos 
+
+y= 3+5 * 2 > 3 and 4== 4 or 3 < 2
+
+# ejemplo 7: con parentesis 
+y= 3+5 != 2*3 and 4== 4 or not 3<2
+
+# ejemplo 8: todo junto
+y= 4**2* 3 < 6 / (7-5) and 7*2 +1==14 or not 3+5 <2
+
