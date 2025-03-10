692b57d5dbc26e6f862de5d23fe2e5950186478e
alan <paolanavas97@gmail.com>
2025-03-10 09:30:19 -0500

    operador and

---
+++ operadores_logicos.py
@@ -0,0 +1,18 @@
+'''
+Operadores logicos 
+Aquellos que operan unicamente 
+con valores booleanos (V F) 
+acorde a las tablas de la verdad 
+'''
+
+# Ejemplo 1:  operador not:
+y= not True 
+print ("El resultado de operar con not es", y)
+
+# Ejemplo 2:  operador and:
+y= True and True 
+print ("El resultado de operar and es", y)
+
+#Ejemplo 3 operador or
+y= True or False
+print ("El resultado de operar con or es", y )pyth
